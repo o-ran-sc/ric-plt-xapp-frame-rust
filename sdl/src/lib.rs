84cd79093c81ce1fc8e94c4c206d71fc3b3d4632
Abhijit Gadgil <gabhijit@iitbombay.org>
2023-03-21 13:02:30 +0530

    sdl: Getting started with SDL crate
    Implementation of Shared Data Layer APIs for the `xapp-framework-rust`.
    Initial skeleton
    
    Signed-off-by: Abhijit Gadgil <gabhijit@iitbombay.org>
    Change-Id: Ia4f883f64ecbd7d4027802614ee33be73d00fc84

---
+++ sdl/src/lib.rs
@@ -0,0 +1,27 @@
+// ==================================================================================
+//   Copyright (c) 2023 Abhijit Gadgil
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// ==================================================================================
+
+//! An Interface to Shared Data Layer (SDL) for the ORAN-SC
+//!
+//! Shared Data Layer (SDL) provides an API for data persistence using backend storages. The
+//! applications can use these API to interface with persistent data.
+//!
+//! This crate defines a common trait `SdlStorage` - this defines the APIs for accessing the stored
+//! data. Additionally included in this crate is an implementation of an actual storage backend
+//! that supports Redis database for persistence. The backend implemented implements the trait
+//! `SdlStorage`.
+//!
+//! Initialization of SDL and backend happens through a set of environment variables.
