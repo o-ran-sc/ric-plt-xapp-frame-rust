e426fffcdde8fbced4b543810f113d8282d9e0f2
Abhijit Gadgil <gabhijit@iitbombay.org>
2023-05-05 10:39:53 +0530

    ric-subscriptions: Adding generated swagger models
    Rust `struct`s for Subscription API models are generated using
    `openapi-generator`. Currently the generated files are being tracked in
    the repository. At some point of time we may have a proper `build.rs`
    support where the files will be generated during build time.
    
    Ignoring the `api` module as we are not using `async` API. This may be
    enabled at future date.
    
    Most of the files are generated by `openapi-tools` rust generator.
    
    Issue-Id: RIC-977
    Change-Id: Ibf3c2297f5cb6cb5a07b38cdf992c3c623cddb46
    Signed-off-by: Abhijit Gadgil <gabhijit@iitbombay.org>

---
+++ api/src/lib.rs
@@ -0,0 +1,7 @@
+// DO NOT USE: `api` module
+// Right now we are only using the generated 'models'. Since the generated `api` are `async` and
+// yet not supported fully, simply a support for `models` is present.
+//
+// pub mod apis;
+
+pub mod models;
